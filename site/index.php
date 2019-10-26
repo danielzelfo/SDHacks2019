4be486108eb82429ccf3bb9adefd2f9ef906cf20
danielzelfo <daniel.zelfo@gmail.com>
2019-10-26 06:02:26 -0700

    first commit

---
+++ site/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Home Page</title>
+</head>
+	
+<body>
+	
+	<form action="http://www.mtsacsdhacks.epizy.com/live.php" method="post">
+		<label for="name">Name:</label>
+		<input id="name" type="text" name="name"><br>
+		<input type="submit">
+	</form>
+	
+</body>
+</html>
